6304612a5f0b985fa6a9ea6ab848a8ce68de11c2
Joanne <joanne60521@gmail.com>
2024-06-05 13:37:28 +0800

    Game Framework_0604
    pull chicken and hold 3 sec and chicken destroy

--- Assets/Scripts/InitiateChicken.cs
+++ Assets/Scripts/InitiateChicken.cs
@@ -2,24 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class InitiateChicken : MonoBehaviour
 {
     public GameObject chicken;
     public Transform _camera;
+    public float timer = 4.0f;
+    public bool create = false;
+    public TextMeshProUGUI myText;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        Instantiate(chicken, transform.position, transform.rotation);
+        Instantiate(chicken, transform.position - new Vector3((float)0.2, 0, 0), transform.rotation);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (create)
+        {
+            Instantiate(chicken, transform.position, transform.rotation);
+            Instantiate(chicken, transform.position - new Vector3((float)0.2, 0, 0), transform.rotation);
+            // myText.text = "press middle finger to catch chicken";
+            create = false;
+        }
         if (Input.GetKeyDown("space"))
         {
-            Instantiate(chicken,new Vector3(transform.position.x, (float)(_camera.position.y-0.3), transform.position.z), transform.rotation);
+            Instantiate(chicken, transform.position, transform.rotation);
+            Instantiate(chicken, transform.position - new Vector3((float)0.2, 0, 0), transform.rotation);
+            // myText.text = "press middle finger to catch chicken";
         }
+
+        // timer -= Time.deltaTime;
+        // if (timer <= 0)
+        // {
+        //     Instantiate(chicken, transform.position, transform.rotation);
+        //     timer = 2.0f;
+        // }
     }
 }
